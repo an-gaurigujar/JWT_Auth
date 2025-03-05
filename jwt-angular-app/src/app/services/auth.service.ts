69872dd93db233b68460c046c9a347770c079916
Gauri Gujar <gauri.g@applicationnexus.com>
2025-03-05 17:43:46 +0530

    Update auth.service.ts

--- jwt-angular-app/src/app/services/auth.service.ts
+++ jwt-angular-app/src/app/services/auth.service.ts
@@ -6,10 +6,9 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000'; 
+  private apiUrl = 'http://localhost:3000'; // Replace with your backend URL
 
   constructor(private http: HttpClient) {}
-
   register(username: string, password: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/register`, { username, password });
   }
@@ -18,7 +17,6 @@ export class AuthService {
     return this.http.post(`${this.apiUrl}/login`, { username, password });
   }
 
- 
   getProtectedData(): Observable<any> {
     return this.http.get(`${this.apiUrl}/protected`);
   }
